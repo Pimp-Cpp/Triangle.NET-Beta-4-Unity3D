9fe5d5e0e228c30fbf19497cc0ab907b90a7f792
Nikolaj Kobek Thorsen <nikolaj@unity-studios.com>
2018-03-02 12:39:16 +0100

    Source code for Unity test scene using Triangle.NET

---
+++ Assets/Triangulation.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using TriangleNet.Geometry;
+using TriangleNet.Meshing;
+using UnityEngine;
+
+public static class Triangulation {
+
+    public static float MinimumAngle { get; set; }
+    public static float MaximumArea { get; set; }
+
+    public static bool Triangulate(List<Vector3> points, List<List<Vector3>> holes, out List<int> outIndices, out List<Vector3> outVertices)
+    {
+        outVertices = new List<Vector3>();
+        outIndices = new List<int>();
+
+        Polygon poly = new Polygon();
+        for (int i = 0; i < points.Count; i++)
+        {
+            float x = points[i].x;
+            float y = points[i].z;
+            poly.Add(new Vertex(x, y));
+
+            if (i == points.Count - 1)
+            {
+                poly.Add(new Segment(new Vertex(x,y),new Vertex(points[0].x,points[0].z)));
+            }
+            else
+            {
+                poly.Add(new Segment(new Vertex(x,y),new Vertex(points[i + 1].x,points[i+1].z)));
+            }
+        }
+
+        for (int i = 0; i < holes.Count; i++)
+        {
+            List<Vertex> vertices = new List<Vertex>();
+            for (int j = 0; j < holes[i].Count; j++)
+            {
+                vertices.Add(new Vertex(holes[i][j].x,holes[i][j].z));
+            }
+            poly.Add(new Contour(vertices),true);
+        }
+        ConstraintOptions constraints = new ConstraintOptions();
+        QualityOptions quality = new QualityOptions();
+        quality.MinimumAngle = MinimumAngle;
+        quality.MaximumArea = MaximumArea;
+        var mesh = poly.Triangulate(constraints,quality);
+
+        foreach (ITriangle t in mesh.Triangles)
+        {
+            for (int j = 2; j >=0; j--)
+            {
+                bool found = false;
+                for (int k = 0; k < outVertices.Count; k++)
+                {
+                    if ((outVertices[k].x == t.GetVertex(j).X) && (outVertices[k].y == t.GetVertex(j).Y))
+                    {
+                        outIndices.Add(k);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    outVertices.Add(new Vector3((float) t.GetVertex(j).X, 0, (float)t.GetVertex(j).Y));
+                    outIndices.Add(outVertices.Count-1);
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Assets/TriangulationTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangulationTest : MonoBehaviour 
+{
+    List<Vector3>  _outline = new List<Vector3>{new Vector3(0,0,0),new Vector3(1,0,0),new Vector3(1,0,1),new Vector3(0.5f,0,1.5f),new Vector3(0,0,1)};
+    List<List<Vector3>>  _holes = new List<List<Vector3>>();
+
+    public Material wireMaterial;
+
+    public float MaximumArea;
+    public float MinimumAngle;
+
+    private float _prevMaxArea;
+    private float _prevMinAngle;
+
+    private GameObject _instantiatedMesh;
+
+    void Start()
+    {
+        TriangulateTestOutline();
+    }
+
+	void TriangulateTestOutline ()
+	{
+	    Debug.Log("Triangulate outline and create mesh");
+        List<int> outIndices;
+	    List<Vector3> outVertices;
+
+	    Triangulation.MaximumArea = MaximumArea;
+	    Triangulation.MinimumAngle = MinimumAngle;
+
+	    Triangulation.Triangulate(_outline, _holes, out outIndices, out outVertices);
+
+	    Mesh triangulatedMesh = new Mesh();
+
+	    triangulatedMesh.vertices = outVertices.ToArray();
+	    triangulatedMesh.triangles = outIndices.ToArray();
+
+	    if (_instantiatedMesh != null)
+	    {
+            Destroy(_instantiatedMesh);
+	    }
+        _instantiatedMesh = new GameObject();
+	    MeshRenderer meshRenderer = _instantiatedMesh.AddComponent<MeshRenderer>();
+	    meshRenderer.material = wireMaterial;
+        MeshFilter meshFilter = _instantiatedMesh.AddComponent<MeshFilter>();
+	    meshFilter.mesh = triangulatedMesh;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (MaximumArea != _prevMaxArea || MinimumAngle != _prevMinAngle)
+	    {
+	        TriangulateTestOutline();
+	        _prevMaxArea = MaximumArea;
+	        _prevMinAngle = MinimumAngle;
+	    }
+	}
+}
